748bc1c37eec978a13aefad7ced1745fa36d05d1
刘凯利 <qq1079265662@163.com>
2022-09-04 16:55:10 +0800

    格式化项目

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 导入清除默认样式css
+import 'reset-css'
+
 const app = createApp(App)
 
 app.use(createPinia())
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,11 @@
 <template>
   <div>123</div>
 </template>
+
+<script lang="ts" setup></script>
+
+<script lang="ts">
+export default {
+  name: 'HoMe'
+}
+</script>
