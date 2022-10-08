4228d0637d8d1f7334bf4cb80d52b4d7bed056b0
刘凯利 <qq1079265662@163.com>
2022-10-08 19:32:23 +0800

    修改hash路由模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/dashboard/index.vue'
 // 导入通用配置
 import { viewSettings } from '@/settings'
@@ -36,7 +36,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
