a14a229f52ffb91545485f24c7dd5c888ebf98ac
刘凯利 <qq1079265662@163.com>
2023-01-30 15:38:12 +0800

    调整着色器

--- src/views/threejs_menu/shaderother/glsl/fragmentShader.glsl
+++ src/views/threejs_menu/shaderother/glsl/fragmentShader.glsl
@@ -6,7 +6,7 @@ varying vec2 vUv;
 
 void main() {
   // 设置片元颜色 r红 g绿 b蓝 a透明度
-  gl_FragColor = vec4(1, 1, 1.0, 1.0); // 每个颜色加点颜色
+  gl_FragColor = vec4(vUv, 1.0, 1.0); // 每个颜色加点颜色
   // 或者直接设置
   // gl_FragColor = vec4(vUv, 1.0, 1.0);
 }
