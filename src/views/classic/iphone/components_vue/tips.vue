157bc7e1e5f0b99d23eb71a1ed754b15104d798c
1079265662 <qq1079265662@163.com>
2023-03-02 22:39:37 +0800

    添加标签

--- src/views/classic/iphone/components_vue/tips.vue
+++ src/views/classic/iphone/components_vue/tips.vue
@@ -1,16 +1,18 @@
 <template>
-  <div>
+  <div
+    class="absolute right-6 bottom-32 flex flex-row-reverse items-center gap-1"
+  >
     <div
-      class="absolute right-6 bottom-32 h-auto max-w-md space-x-2 break-all rounded-xl bg-sky-400 p-2.5 shadow-md ring-4 ring-sky-300 ring-opacity-75 backdrop-blur-sm"
+      class="h-auto max-w-md space-x-2 break-all rounded-xl bg-sky-400 p-1.5 shadow-md ring-4 ring-sky-300 ring-opacity-75 backdrop-blur-sm"
     >
-      <h1 class="text-xl">摄像头</h1>
+      <h1 class="text-lg font-bold">摄像头</h1>
 
-      <h2>
+      <h2 class="mt-1 align-middle text-sm">
         5000万三摄系统，全景、广角、微距技能任意释放。 三星GN5超感光主摄，1/1.57
         英寸超大底， 无论抓拍、夜拍，随手拍都能清晰呈现。
       </h2>
-      <!-- <h2></h2> -->
     </div>
+    <div class="h-0.5 w-28 bg-sky-400">123</div>
   </div>
 </template>
 
